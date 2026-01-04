6b2146efb8f184707d879cc22722e0a031f7a6d3
Zelhad <ze.elhadraoui@gmail.com>
2026-01-04 21:34:41 +0100

    I want to get back to streams for  now we will see the collections after the streams tasks for now

--- src/javaVersionsCores/collectionsApi/demo1.java
+++ src/javaVersionsCores/collectionsApi/demo1.java
@@ -4,16 +4,18 @@ import java.util.ArrayList;
 
 public class demo1 {
 	public static void main(String[] args) {
+		
 		ArrayList<String> productNames = new ArrayList<>();
+		
 		productNames.add("Phones");
 		productNames.add("Electronics");
 		productNames.add("Smart tvs");
 		productNames.add("Children");
 		productNames.add("Womens");
+
+	
 		
-		for(String productName : productNames) {
-			System.out.println(productName);
-		}
+		
 		
 	}
 }
